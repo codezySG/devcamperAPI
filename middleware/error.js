c1299ef401b1303ec3f4b233ee7b9ad478272a21
stefanagloginic <stefana.gloginic@gmail.com>
2020-06-02 01:00:49 -0700

    [setup] add duplicate and validation error handling

--- middleware/error.js
+++ middleware/error.js
@@ -5,12 +5,18 @@ export const errorHandler = (err, req, res, next) => {
 	console.log(err.stack.red);
 	let error = { ...err };
 	error.message = err.message;
-	const { name, value } = error;
+	const { name, value, code } = error;
 
 	// mongoose bad obj id
 	if (name === 'CastError') {
 		const message = `Resource not found with id of ${value}`;
 		error = new ErrorResponse(message, 404);
+	} else if (code === 11000) { // mongoose duplicate key
+		const message = 'Duplicate field value entered';
+		error = new ErrorResponse(message, 400); // bad client request
+	} else if (name === 'ValidationError') { // validation error
+		const message = Object.values(err.errors).map(({message}) => {message});
+		error = new ErrorResponse(message, 400); // bad client request
 	}
 
 	res.status(error.statusCode || 500).json({
