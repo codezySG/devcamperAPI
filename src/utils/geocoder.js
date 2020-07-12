24ff6b0f12cc092c031f022e7292f073ee18910f
codezySG <codezySG@gmail.com>
2020-07-11 22:49:12 -0700

    [fixLocalDev] fix the local dev

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -1,7 +1,4 @@
 import NodeGeocoder from 'node-geocoder';
-import dotenv from 'dotenv';
-
-dotenv.config({path: './configs/config.env'});
 
 const options = {
 	provider: process.env.GEOCODER_PROVIDER,
